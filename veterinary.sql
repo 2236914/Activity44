08d9c7307d20f05d78871ccb39ee177363febb81
Faith Banares <22-36914@g.batstate-u.edu.ph>
2023-11-08 11:48:02 +0800

    3-7

--- veterinary.sql
+++ veterinary.sql
@@ -23,3 +23,58 @@ CREATE TABLE animals (
     FOREIGN KEY (ownerid) REFERENCES owners(PK)
 );
 
+--3	Create new branch named "feat/create-table-appointments"
+CREATE TABLE appointments (
+    appointed SERIAL PRIMARY KEY,
+    animalid INT,
+    appointdate DATE,
+    reason VARCHAR(255),
+    FOREIGN KEY (animalid) REFERENCES animals(animalid)
+);
+
+--4	Create new branch named "feat/create-table-doctors"
+CREATE TABLE doctors (
+    doctorid SERIAL PRIMARY KEY,
+    dfirstname VARCHAR(50),
+    dlastname VARCHAR(50),
+    specialty VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
+
+--5	Create new branch named "feat/create-table-invoices"
+CREATE TABLE invoices (
+    invoiceid SERIAL PRIMARY KEY,
+    appointed INT,
+    totalamount NUMERIC(10, 2),
+    paymentdate DATE,
+    FOREIGN KEY (appointed) REFERENCES appointments(appointed)
+);
+
+--6	Create new branch named "feat/create-table-medicalrecords"
+CREATE TABLE medicalrecords (
+    recorded SERIAL PRIMARY KEY,
+    animalid INT,
+    recorddate TIMESTAMP,
+    doctorid INT,
+    diagnosis TEXT,
+    prescription TEXT,
+    notes TEXT,
+    FOREIGN KEY (animalid) REFERENCES animals(animalid),
+    FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
+);
+
+--7. insert owners
+INSERT INTO owners (ownerid, ofirstname, olastname, address, phone, email)
+VALUES
+    (1, 'Juan', 'Dela Cruz', '123 Main St, Manila', '123-456-7890', 'juan@example.com'),
+    (2, 'Maria', 'Santos', '456 Eim St, Quezon City', '987-654-3210', 'maria@example.com'),
+    (3, 'Antonio', 'Gonzales', '789 Oak St, Cebu', '555-123-45678', 'antonio@example.com'),
+    (4, 'Elena', 'Rodriguez', '345 Pine St, Davao', '777-888-9999', 'elena@example.com'),
+    (5, 'Ricardo', 'Lim', '987 Cedar St, Makati', '222-333-4444', 'ricardo@example.com'),
+    (6, 'Isabela', 'Reyes', '111 Acacia St, Pasig', '999-000-1111', 'isabel@example.com'),
+    (7, 'Luis', 'Torres', '555 Maple St, Mandaluyong', '123-555-7777', 'luis@example.com'),
+    (8, 'Carmen', 'Fernandez', '222 Birch St, Taguig', '333-222-1111', 'carmen@example.com'),
+    (9, 'Pedro', 'Santillian', '888, Spruce St, Bacolod', '888-777-3333', 'pedro@example.com'),
+    (10, 'Sofia', 'Villianueva', '777 Walnut St, Iloilo', '111-999-3333', 'sofia@example.com');
+
