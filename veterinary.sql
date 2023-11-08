e20753707db11f31dbc16ea1b5632397db4f907e
adriancamota <22-39287@g.batstate-u.edu.ph>
2023-11-08 10:10:13 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -10,3 +10,16 @@ CREATE TABLE owners (
     email VARCHAR(100)
 );
 
+--2.	Create new branch named "feat/create-table-animals"
+CREATE TABLE animals (
+    animalid SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    dateofbirth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+    ownerid INT,
+    FOREIGN KEY (ownerid) REFERENCES owners(PK)
+);
+
