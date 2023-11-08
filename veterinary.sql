7e1478fb24a12052a8bdcd0f7a7c67d92268604e
Faith Banares <22-36914@g.batstate-u.edu.ph>
2023-11-08 09:56:43 +0800

    feat create table owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,2 +1,12 @@
 CREATE DATABASE veterinary;
 
+--1.	Create new branch named "feat/create-table-owners
+CREATE TABLE owners (
+    PK SERIAL PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
+
